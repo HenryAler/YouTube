c022660a8ad7fa979e6f0ea20eea3b458d8f6bf4
HenryAler <114738013+HenryAler@users.noreply.github.com>
2022-11-01 00:06:45 +0300

    Add some code

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import *
 from ui import Ui_mainWindow
-import requests
+from pytube import YouTube
 
 
 class MainProject(QtWidgets.QMainWindow):
@@ -9,20 +9,29 @@ class MainProject(QtWidgets.QMainWindow):
         super(MainProject, self).__init__()
         self.ui = Ui_mainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.download_video)
+        self.ui.lineEdit.setFocus()
+        self.ui.progressBar.setValue(0)
 
-        self.ui.toolButton.clicked.connect(self.open_dialog)
+    def download_video(self):
 
-    def open_dialog(self):
-        res = QFileDialog.getExistingDirectory(self, "Выбрать папку", ".")
-        print(res)
-
-    def get_files(self):
-        pass
-
+        try:
+            path = QFileDialog.getExistingDirectory(self, "Выбрать папку", "/", options=QFileDialog.ShowDirsOnly)
+            url = self.ui.lineEdit.text()
+            yt = YouTube(url)
+            title = yt.title
+            yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download(
+                f"{path}")
+            self.ui.label.setText("Downloaded..." + title)
+            self.ui.progressBar.setValue(100)
+            return title
+        except Exception:
+            self.ui.label.setText("Некорректная ссылка")
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = QtWidgets.QMainWindow()
     rex = MainProject()
